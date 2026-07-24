387dea8945b40e674023741b3ece924be770826b
krishnasingh6398 <krishna.singh.cse.2025@miet.ac.in>
2026-07-24 12:25:22 +0530

    Add files via upload

--- insertion_array.c
+++ insertion_array.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 void main(){
-    int arr[20],n,pos,item;
-    printf("Enter no of elements\n")
+    int i,arr[20],n,pos,item;
+    printf("Enter no of elements: ");
     scanf("%d",&n);
-    printf("Enter the elements\n")
+    printf("Enter the elements\n");
     for ( i = 0; i < n; i++){
-       scanf("%d",arr[i]);
+       scanf("%d", &arr[i]);
     }
-    printf("Array elements are\n")
+    printf("Array elements are\n");
     for ( i = 0; i < n; i++){
-       printf("%d",arr[i]);
+       printf("%d ",arr[i]);
     }
-    printf("Enter the position of element")
+    printf("Enter the position of element");
     scanf("%d",&pos);
-    printf("Enter the element")
+    printf("Enter the element");
     scanf("%d",&item);
 
     for ( i = n; i >=pos; i--)
@@ -21,6 +21,10 @@ void main(){
       arr[i] = arr[i-1];
     }
     arr[pos-1]= item;
-    
-    
+    n++;
+    for ( i = 0; i < n; i++){
+       printf("%d ",arr[i]);
+    }
+    arr[pos-1]= item;
+
 }
