107cb26eea59b0e60f3afef766441a1fa5b614a4
krishnasingh6398 <krishna.singh.cse.2025@miet.ac.in>
2026-07-31 12:19:51 +0530

    Add files via upload

---
+++ Multiplication of matrices.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+int main(){
+    int a[10][10],b[10][10],c[10][10];
+    int r1,c1,r2,c2;
+    printf("Enter row and coloumn of 1st matrix: ");
+    scanf("%d %d", &r1, &c1);
+        printf("Enter row and coloumn of 2nd matrix: ");
+    scanf("%d %d", &r2, &c2);
+
+    if (c1==r2)
+    {
+        printf("Enter elements of 1st matrix: \n");
+        for(int i=0; i<r1;i++)
+        {
+            for(int j=0; j<c1;j++){
+                scanf("%d",&a[i][j]);
+            }
+        }
+         printf("Enter elements of 2nd matrix: \n");
+        for(int i=0; i<r2;i++)
+        {
+            for(int j=0; j<c2;j++){
+                scanf("%d",&b[i][j]);
+            }
+        }   
+        printf("Multiplication matrix: \n");
+         for(int i=0; i<r1;i++){
+             for(int j=0; j<c2;j++)
+            {
+                for(int j=0; j<c2;j++)      
+                {
+                    c[i][j]=0; 
+                    for (int k = 0;k<c1;k++)
+                    c[i][j]+= (a[i][j] * b[k][j]);
+                }  
+            }
+
+         }
+
+         for(int i=0; i<r1;i++)
+        {
+            for(int j=0; j<c2;j++)
+            {
+                printf("%d ", c[i][j]);
+            }
+            printf("\n");
+        }
+
+    }
+    else{
+        printf("Matrix cannot be added due to unequal order ");
+    }
+
+
+
+  
